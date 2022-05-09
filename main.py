674adea30e8f423542b9470fa35966e3d8e964b3
Raymond Vigil <97466696+Vigil20@users.noreply.github.com>
2022-05-09 04:07:46 +0000

    dropdown created

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import tkinter as tk
 
 #functionality
-
-
-
-
 # Layout 
 window = tk.Tk()
 window.title("Feet to Meters")
@@ -14,7 +10,7 @@ feetLabel = tk.Label(text="Feet")
 feetLabel.pack()
 feetLabel.place(x=250, y=60)
 
-meterLabel = tk.Label(text="Meter")
+meterLabel = tk.Label(text="distance")
 meterLabel.pack()
 meterLabel.place(x=250, y= 100)
 
@@ -29,3 +25,11 @@ entry.place(x = 145, y = 50, height = 30, width = 100)
 calculateButton = tk.Button(text ="calculate")
 calculateButton.pack()
 calculateButton.place(x=250, y=150)
+ #dropdown menu 
+measurementList = ["inches", "Feet","meters","Kilometers"]
+measurementMenu = tk.StringVar(window)
+measurementMenu.set(measurementList[0])
+
+options = tk.OptionMenu(window, measurementMenu, *measurementList)
+options.pack()
+options.place(x= 275, y=65)
